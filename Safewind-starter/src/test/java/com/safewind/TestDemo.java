a5899751a3ad8ca05cca7fd76393b7b00d0a7926
Darven <1926248272@qq.com>
2025-05-21 15:44:17 +0800

    单元测试

---
+++ Safewind-starter/src/test/java/com/safewind/TestDemo.java
@@ -0,0 +1,18 @@
+package com.safewind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: Darven
+ * @CreateTime: 2025-05-21  15:35
+ * @Description: TODO
+ */
+@SpringBootTest
+public class TestDemo {
+
+    @Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
