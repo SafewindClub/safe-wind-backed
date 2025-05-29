31bfdaacc671584902a83c21fb7570d5608b77fc
Darven <1926248272@qq.com>
2025-05-29 08:47:07 +0800

    [update] -新增新闻模块字段

--- Safewind-infra/Safewind-infra-basic/src/main/java/com/safewind/infra/basic/entity/WsNews.java
+++ Safewind-infra/Safewind-infra-basic/src/main/java/com/safewind/infra/basic/entity/WsNews.java
@@ -1,5 +1,9 @@
 package com.safewind.infra.basic.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.io.Serializable;
 
@@ -9,6 +13,8 @@ import java.io.Serializable;
  * @author Darven
  * @since 2025-05-21 21:46:55
  */
+@Setter
+@Getter
 public class WsNews implements Serializable {
     private static final long serialVersionUID = -49098484929198511L;
 
@@ -25,6 +31,14 @@ public class WsNews implements Serializable {
      * 新闻内容
      */
     private String content;
+    /**
+     * 新闻内容/html
+     * */
+    private String htmlContent;
+    /**
+     * 封面
+     * */
+    private String coverImg;
     /**
      * 删除标志（0代表存在 2代表删除）
      */
@@ -46,78 +60,6 @@ public class WsNews implements Serializable {
      */
     private LocalDateTime updateTime;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getTitle() {
-        return title;
-    }
-
-    public void setTitle(Long title) {
-        this.title = title;
-    }
-
-    public LocalDateTime getPublishTime() {
-        return publishTime;
-    }
-
-    public void setPublishTime(LocalDateTime publishTime) {
-        this.publishTime = publishTime;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getDelFlag() {
-        return delFlag;
-    }
-
-    public void setDelFlag(String delFlag) {
-        this.delFlag = delFlag;
-    }
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
 
 }
 
